e6083d10cc10cd6cb25b2898b299bbb7f9e957ad
Sammy Albasateneh <salbasateneh@lazydogrestaurants.com>
2020-02-05 16:00:39 -0800

    reverted to first creation of object

--- public/js/app.js
+++ public/js/app.js
@@ -37,21 +37,18 @@ const params = {
   };
 
   const merakiNetworks = meraki.NetworksController.getOrganizationNetworks(params)
+  
   let newArr = [];
+  
   async function getNetworksandNames() {
       const response = await merakiNetworks
       const data = await response
      return exececuteGetNetworksandNames(data);
-    //   data.forEach(function(data) {
-    //       const name = data.name
-    //       const id = data.id
-    //       newArr.push(name, id)
-    //       console.log(newArr)
-    //   })
+
   }
-  
-function exececuteGetNetworksandNames(data) {
-    var newArr = [];
+
+  function exececuteGetNetworksandNames(data) {
+    
     data.forEach(function(res) {
         var id = res.id
         var name = res.name
@@ -63,37 +60,13 @@ function exececuteGetNetworksandNames(data) {
 }
 
 getNetworksandNames();
-//     .then(function(res) {
-   
-// res.forEach(function(data) {
-//     console.log(data.id, data.name)
-    
-// })
 
-//     })
-    
-//     .catch(e => console.log(e));
-
-
-
-// Get DHCPHandling
-// let networkId = 'N_651333096108460350';
-// const promise = controller.getNetwork_vlans(networkId);
-// promise.then((response) => {
-
-//     response.forEach(function(response) {
-//         var dhcpHandling = response.dhcpHandling;
-//         console.log("Plano: " + dhcpHandling)
-//     })
-   
-// },(err) => {
-//     console.log(err)
-// })
 
 
 // Network Devices
 
-// let params = [];
+// function getSerial () {
+//   let params = [];
 // let networkId = "N_651333096108465793"; // Sandbox Campus-SFO
 // params["networkId"] = networkId;
 
@@ -103,25 +76,9 @@ getNetworksandNames();
 //   })
 //   .catch(err => {
 //     console.log(err);
-//   });
+//   });  
+// }
 
 
 
 
-// Get Device Clients
-
-// let params = {};
-// let serial = 'Q2LD-UZYA-TWAA';
-
-// params['serial'] = serial
-
-// let timespan = '2000'
-// params['timespan'] = timespan
-
-// meraki.ClientsController.getDeviceClients(params)
-// .then(res => {
-//     console.log(res);
-// })
-// .catch(err => {
-//     console.log(err)
-// })

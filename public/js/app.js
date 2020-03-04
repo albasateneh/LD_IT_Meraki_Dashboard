5dbff9f3c759fe292161393656f0ff22dd70d029
Sammy Albasateneh <salbasateneh@lazydogrestaurants.com>
2020-03-04 12:02:49 -0800

    printed location and security appliance data to page BASIC

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
   $("#button").on("click", function() {
-    alert("Clicked Foo")
+    alert("clicked")
+   $.get("/api/data", function(data) {
+  data.forEach(function(res) {
+    console.log(res)
+   
+     var listItem = $("<li class='list-group-item mt-4'>");
+
+     listItem.append(
+      $("<h3>").text(res[2].name),
+      $("<hr>"),
+
+      $("<li>").text("Interface: " + res[0].interface, ",", "Status: " + res[0].status, ",", "Public IP: " +  res[0].publicIp),
+      $("<li>").text("Status: " + res[0].status),
+      $("<li>").text("Public IP: " +  res[0].publicIp),
+      $("<li>").text("Using Static: " + res[0].usingStaticIp),
+      $("<hr>"),
+      $("<li>").text("Interface: " + res[1].interface, ",", "Status: " + res[0].status, ",", "Public IP: " +  res[0].publicIp),
+      $("<li>").text("Status: " + res[1].status),
+      $("<li>").text("Public IP: " +  res[1].publicIp),
+      $("<li>").text("Using Static: " + res[1].usingStaticIp),
+      
+
+     );
+        $("#body").append(listItem)
+
+   
+    })
   })
-})
+
+ 
+      
+     
+   })
+  })

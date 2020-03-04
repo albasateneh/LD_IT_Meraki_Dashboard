d659e56e95ca71fe6150aab5efce0a945ebe78a3
Sammy Albasateneh <salbasateneh@lazydogrestaurants.com>
2020-03-04 12:12:12 -0800

    added next steps

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $("#button").on("click", function() {
-    alert("clicked")
    $.get("/api/data", function(data) {
   data.forEach(function(res) {
     console.log(res)

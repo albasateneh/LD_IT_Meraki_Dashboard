cd5990f051c4493ebae54260d4f01a8d51f495b4
Sammy Albasateneh <salbasateneh@lazydogrestaurants.com>
2020-08-25 09:54:36 -0700

    cleaned email.js

--- public/js/email.js
+++ public/js/email.js
@@ -1,41 +1,5 @@
 const axios = require("axios");
-var nodemailer = require('nodemailer');
 var nodeoutlook = require('nodejs-nodemailer-outlook')
-nodeoutlook.sendEmail({
-  auth: {
-    user: "salbasateneh@lazydogrestaurants.com",
-    pass: "rvpzmdmqghrtpypn"
-  },
-  from: 'salbasateneh@lazydogrestaurants.com',
-  to: 'salbasateneh@lazydogrestaurants.com',
-  subject: 'hey',
-  text: 'hey',
-  onError: (e) => console.log(e),
-    onSuccess: (i) => console.log(i)
-})
-// var transporter = nodemailer.createTransport({
-//   service: 'Gmail',
-//   auth: {
-//     user: 'sam.albasateneh@gmail.com',
-//     pass: 'meteora!'
-//   }
-// })
-// console.log('created')
-// var mailOptions = {
-//   from: 'sam.albasateneh@gmail.com',
-//   to: 'salbasateneh@lazydogrestaurants.com',
-//   subject: 'TEST',
-//   text: 'test'
-// }
-
-// transporter.sendMail(mailOptions, function(error, info) {
-//   if (error) {
-//     console.log(error);
-//   }
-//   else {
-//     console.log('Email sent: ' + info.response);
-//   }
-// })
 
 
 
@@ -51,49 +15,31 @@ axios.get(baseURL + "/api/data")
       var interface = res[1].interface
       var status = res[1].status
       if (res[1].status != "Ready" && !str.includes("GUEST") && !str.includes("Guest") && res[1].status != "Active") {
-        console.log(res[2].name + " | " + res[1].interface + " " + res[1].status + ". ")
+        // console.log(res[2].name + " | " + res[1].interface + " " + res[1].status + ". ")
         temp.push({ str, interface, status })
       }
       return temp
     })
     // console.log(temp)
-    temp.forEach(function(res) {
+    temp.forEach(function (res) {
       console.log(res.str + " | " + res.interface + " " + res.status)
-      // var transporter = nodemailer.createTransport({
-      //   service: 'Gmail',
-      //   auth: {
-      //     user: 'sam.albasateneh@gmail.com',
-      //     pass: 'meteora!'
-      //   }
-      // })
-      // let transporter = nodemailer.createTransport({
-      //   service: "Outlook365",
-      //   auth: {
-      //     user: 'salbasateneh@lazydogrestaurants.com',
-      //     pass: 'Welcome1'
-      //   },    
-      // })
-      // console.log('created')
-      // var mailOptions = {
-      //   from: 'salbasateneh@lazydogrestaurants.com',
-      //   to: 'salbasateneh@lazydogrestaurants.com',
-      //   subject: res.str + " | " + res.interface + "  " + res.status,
-      //   text: res.str + " | " + res.interface + "  " + res.status
-      // }
-      
-      // transporter.sendMail(mailOptions, function(error, info) {
-      //   if (error) {
-      //     console.log(error);
-      //   }
-      //   else {
-      //     console.log('Email sent: ' + info.response);
-      //   }
-      // })
+      nodeoutlook.sendEmail({
+        auth: {
+          user: "salbasateneh@lazydogrestaurants.com",
+          pass: "rvpzmdmqghrtpypn"
+        },
+        from: 'salbasateneh@lazydogrestaurants.com',
+        to: 'salbasateneh@lazydogrestaurants.com',
+        subject: res.str + " | " + res.interface + "  " + res.status,
+        text: res.str + " | " + res.interface + "  " + res.status,
+        onError: (e) => console.log(e),
+        onSuccess: (i) => console.log(i)
+      })
+   
     })
-    
+
 
   })
-        // .catch(e => console.log(e))
 
 
 

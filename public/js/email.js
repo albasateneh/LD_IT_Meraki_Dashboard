1c6a1d6795c38c97cb981986395d35c4b707b687
Sammy Albasateneh <salbasateneh@lazydogrestaurants.com>
2020-08-25 15:10:34 -0700

    added email automation code to server.js and tested

--- public/js/email.js
+++ public/js/email.js
@@ -4,12 +4,48 @@ var nodeoutlook = require('nodejs-nodemailer-outlook')
 
 
 const baseURL = "http://localhost:8080"
-var temp = [];
 
-axios.get(baseURL + "/api/data")
+function run() {
+
+  axios.get(baseURL + "/api/data")
   .then(function (response) {
     var resData = response.data
+    var resData2 = response.data
+    var temp = [];
+    var temp2 = [];
+resData2.forEach(function(res2) {
+  var str = res2[2].name
+  var interface = res2[0].interface;
+  var status = res2[0].status
+  if (res2[0].status != "Ready" && !str.includes("GUEST") && !str.includes("Guest") && res2[0].status != "Active") {
+          // console.log(res[2].name + " | " + res[1].interface + " " + res[1].status + ". ")
+          temp2.push({ str, interface, status })
+        }
+        return temp2
+      })
+      // console.log(temp)
+      temp2.forEach(function (res) {
+        console.log(res.str + " | " + res.interface + " " + res.status)
+        nodeoutlook.sendEmail({
+          auth: {
+            user: "salbasateneh@lazydogrestaurants.com",
+            pass: "rvpzmdmqghrtpypn"
+          },
+          from: 'salbasateneh@lazydogrestaurants.com',
+          to: 'salbasateneh@lazydogrestaurants.com',
+          subject: res.str,
+          text: res.interface + " " + res.status,
+          onError: (e) => console.log(e),
+          onSuccess: (i) => console.log(i)
+        })
+     
+      })
+    })
 
+    axios.get(baseURL + "/api/data")
+    .then(function (response) {
+      var resData = response.data
+      var temp = [];
     resData.forEach(function (res) {
       var str = res[2].name
       var interface = res[1].interface
@@ -30,18 +66,20 @@ axios.get(baseURL + "/api/data")
         },
         from: 'salbasateneh@lazydogrestaurants.com',
         to: 'salbasateneh@lazydogrestaurants.com',
-        subject: res.str + " | " + res.interface + "  " + res.status,
-        text: res.str + " | " + res.interface + "  " + res.status,
+        subject: res.str,
+        text: res.interface + " " + res.status,
         onError: (e) => console.log(e),
         onSuccess: (i) => console.log(i)
       })
    
     })
 
-
   })
+  
 
+}
 
+run()
 
 
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 var express = require('express');
 const meraki = require("meraki");
 const axios = require("axios");
+var nodeoutlook = require('nodejs-nodemailer-outlook')
 
 // ================================================================================
 // Get Key
@@ -226,6 +227,79 @@ async function merge2() {
 
 }
 
+function runEmailAuto() {
+
+  axios.get(baseURL + "/api/data")
+  .then(function (response) {
+    var resData = response.data
+    var temp = [];
+
+    resData.forEach(function (res) {
+      var str = res[2].name
+      var interface = res[1].interface
+      var status = res[1].status
+      if (res[1].status != "Ready" && !str.includes("GUEST") && !str.includes("Guest") && res[1].status != "Active") {
+        // console.log(res[2].name + " | " + res[1].interface + " " + res[1].status + ". ")
+        temp.push({ str, interface, status })
+      }
+      return temp
+    })
+    // console.log(temp)
+    temp.forEach(function (res) {
+      console.log(res.str + " | " + res.interface + " " + res.status)
+      nodeoutlook.sendEmail({
+        auth: {
+          user: "salbasateneh@lazydogrestaurants.com",
+          pass: "rvpzmdmqghrtpypn"
+        },
+        from: 'salbasateneh@lazydogrestaurants.com',
+        to: 'salbasateneh@lazydogrestaurants.com',
+        subject: res.str,
+        text: 'Store Meraki: ' + res.interface + " " + res.status,
+        onError: (e) => console.log(e),
+        onSuccess: (i) => console.log(i)
+      })
+   
+    })
+
+
+  })
+
+  axios.get(baseURL + "/api/data")
+  .then(function (response) {
+    var resData2 = response.data
+    var temp2 = [];
+resData2.forEach(function(res2) {
+  var str = res2[2].name
+  var interface = res2[0].interface;
+  var status = res2[0].status
+  if (res2[0].status != "Ready" && !str.includes("GUEST") && !str.includes("Guest") && res2[0].status != "Active") {
+          // console.log(res[2].name + " | " + res[1].interface + " " + res[1].status + ". ")
+          temp2.push({ str, interface, status })
+        }
+        return temp2
+      })
+      // console.log(temp)
+      temp2.forEach(function (res) {
+        console.log(res.str + " | " + res.interface + " " + res.status)
+        nodeoutlook.sendEmail({
+          auth: {
+            user: "salbasateneh@lazydogrestaurants.com",
+            pass: "rvpzmdmqghrtpypn"
+          },
+          from: 'salbasateneh@lazydogrestaurants.com',
+          to: 'salbasateneh@lazydogrestaurants.com',
+          subject: res.str,
+          text: 'Store Meraki: ' + res.interface + " " + res.status,
+          onError: (e) => console.log(e),
+          onSuccess: (i) => console.log(i)
+        })
+     
+      })
+    })
+
+}
+
 // ================================================================================
 // Post to Server
 // ================================================================================  
@@ -238,6 +312,7 @@ async function dataSet() {
   console.log(data)
   console.log("Posted!")
   axios.post(baseURL + '/api/data', data)
+  runEmailAuto();
   // .catch(e => console.log(e))
 }
 

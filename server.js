78d1af73a97aa46ba36ebf587127075e59877959
Sammy Albasateneh <salbasateneh@lazydogrestaurants.com>
2020-08-26 13:33:45 -0700

    tested this morning and everything works

--- server.js
+++ server.js
@@ -227,53 +227,57 @@ async function merge2() {
 
 }
 
+// ================================================================================
+// Create Email Notification
+// ================================================================================ 
+
 function runEmailAuto() {
 
   axios.get(baseURL + "/api/data")
-  .then(function (response) {
-    var resData = response.data
-    var temp = [];
+    .then(function (response) {
+      var resData = response.data
+      var temp = [];
 
-    resData.forEach(function (res) {
-      var str = res[2].name
-      var interface = res[1].interface
-      var status = res[1].status
-      if (res[1].status != "Ready" && !str.includes("GUEST") && !str.includes("Guest") && res[1].status != "Active") {
-        // console.log(res[2].name + " | " + res[1].interface + " " + res[1].status + ". ")
-        temp.push({ str, interface, status })
-      }
-      return temp
-    })
-    // console.log(temp)
-    temp.forEach(function (res) {
-      console.log(res.str + " | " + res.interface + " " + res.status)
-      nodeoutlook.sendEmail({
-        auth: {
-          user: "notifications@lazydogrestaurants.com",
-            pass: "jVY4s84s"
-        },
-        from: 'notifications@lazydogrestaurants.com',
-        to: 'iit@lazydogrestaurants.com',
-        subject: 'Alert for ' + res.str + ' - ' + res.interface + ' ' + res.status,
-        text: 'Store Meraki: ' + res.interface + " " + res.status,
-        onError: (e) => console.log(e),
-        onSuccess: (i) => console.log(i)
+      resData.forEach(function (res) {
+        var str = res[2].name
+        var interface = res[1].interface
+        var status = res[1].status
+        if (res[1].status != "Ready" && !str.includes("GUEST") && !str.includes("Guest") && res[1].status != "Active") {
+          // console.log(res[2].name + " | " + res[1].interface + " " + res[1].status + ". ")
+          temp.push({ str, interface, status })
+        }
+        return temp
       })
-   
+      // console.log(temp)
+      temp.forEach(function (res) {
+        console.log(res.str + " | " + res.interface + " " + res.status)
+        nodeoutlook.sendEmail({
+          auth: {
+            user: "notifications@lazydogrestaurants.com",
+            pass: "jVY4s84s"
+          },
+          from: 'notifications@lazydogrestaurants.com',
+          to: 'iit@lazydogrestaurants.com',
+          subject: 'Alert for ' + res.str + ' - ' + res.interface + ' ' + res.status,
+          text: 'Store Meraki: ' + res.interface + " " + res.status,
+          onError: (e) => console.log(e),
+          onSuccess: (i) => console.log(i)
+        })
+
+      })
+
+
     })
-
-
-  })
 
   axios.get(baseURL + "/api/data")
-  .then(function (response) {
-    var resData2 = response.data
-    var temp2 = [];
-resData2.forEach(function(res2) {
-  var str = res2[2].name
-  var interface = res2[0].interface;
-  var status = res2[0].status
-  if (res2[0].status != "Ready" && !str.includes("GUEST") && !str.includes("Guest") && res2[0].status != "Active") {
+    .then(function (response) {
+      var resData2 = response.data
+      var temp2 = [];
+      resData2.forEach(function (res2) {
+        var str = res2[2].name
+        var interface = res2[0].interface;
+        var status = res2[0].status
+        if (res2[0].status != "Ready" && !str.includes("GUEST") && !str.includes("Guest") && res2[0].status != "Active") {
           // console.log(res[2].name + " | " + res[1].interface + " " + res[1].status + ". ")
           temp2.push({ str, interface, status })
         }
@@ -294,7 +298,7 @@ resData2.forEach(function(res2) {
           onError: (e) => console.log(e),
           onSuccess: (i) => console.log(i)
         })
-     
+
       })
     })
 
